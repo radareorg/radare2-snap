f6f1952981d7b40f4979e6959cd2ca196b7f210d
Pau RE <prodrigestivill@gmail.com>
2024-03-26 16:10:10 +0100

    Simplify docker metadata file

--- metadata.mk
+++ metadata.mk
@@ -1,38 +1,49 @@
-DOCKER_DESCRIPTION:=UNIX-like reverse engineering framework and command-line toolset
-DOCKER_LABELS:= \
-		--label org.opencontainers.image.title="radare2" \
+DOCKER_TITLE=radare2
+DOCKER_DESCRIPTION=UNIX-like reverse engineering framework and command-line toolset
+DOCKER_VENDOR=radareorg
+DOCKER_LICENSES=LGPL-3.0-only
+DOCKER_HOMEPAGE_URL=https://www.radare.org/
+DOCKER_DOCUMENTATION_URL=https://github.com/radareorg/radare2-snap/blob/main/README-containers.md
+DOCKER_SOURCE_URL=https://github.com/radareorg/radare2-snap
+
+# Docker labels included inside docker image config
+DOCKER_LABELS= \
+		--label org.opencontainers.image.title="$(DOCKER_TITLE)" \
 		--label org.opencontainers.image.description="$(DOCKER_DESCRIPTION)" \
 		--label org.opencontainers.image.ref.name="$(REGISTRY_IMAGE)" \
 		--label org.opencontainers.image.version="$(R2_VERSION)" \
-		--label org.opencontainers.image.vendor="radareorg" \
-		--label org.opencontainers.image.licenses="LGPL-3.0-only" \
-		--label org.opencontainers.image.url="https://www.radare.org/" \
-		--label org.opencontainers.image.documentation="https://github.com/radareorg/radare2-snap/blob/main/README-containers.md" \
-		--label org.opencontainers.image.source="https://github.com/radareorg/radare2-snap" \
+		--label org.opencontainers.image.vendor="$(DOCKER_VENDOR)" \
+		--label org.opencontainers.image.licenses="$(DOCKER_LICENSES)" \
+		--label org.opencontainers.image.url="$(DOCKER_HOMEPAGE_URL)" \
+		--label org.opencontainers.image.documentation="$(DOCKER_DOCUMENTATION_URL)" \
+		--label org.opencontainers.image.source="$(DOCKER_SOURCE_URL)" \
 		--label org.opencontainers.image.revision="$(R2_SNAP_COMMIT)" \
 		--label org.opencontainers.image.base.name="$(BASE_IMAGE)"
 
-DOCKER_ANNOTATIONS_MANIFEST:= \
-		--annotation manifest:org.opencontainers.image.title="radare2" \
+# Docker annotations for the repository manifest for the platform image
+DOCKER_ANNOTATIONS_MANIFEST= \
+		--annotation manifest:org.opencontainers.image.title="$(DOCKER_TITLE)" \
 		--annotation manifest:org.opencontainers.image.description="$(DOCKER_DESCRIPTION)" \
 		--annotation manifest:org.opencontainers.image.ref.name="$(REGISTRY_IMAGE)" \
 		--annotation manifest:org.opencontainers.image.version="$(R2_VERSION)" \
-		--annotation manifest:org.opencontainers.image.vendor="radareorg" \
-		--annotation manifest:org.opencontainers.image.licenses="LGPL-3.0-only" \
-		--annotation manifest:org.opencontainers.image.url="https://www.radare.org/" \
-		--annotation manifest:org.opencontainers.image.documentation="https://github.com/radareorg/radare2-snap/blob/main/README-containers.md" \
-		--annotation manifest:org.opencontainers.image.source="https://github.com/radareorg/radare2-snap" \
+		--annotation manifest:org.opencontainers.image.vendor="$(DOCKER_VENDOR)" \
+		--annotation manifest:org.opencontainers.image.licenses="$(DOCKER_LICENSES)" \
+		--annotation manifest:org.opencontainers.image.url="$(DOCKER_HOMEPAGE_URL)" \
+		--annotation manifest:org.opencontainers.image.documentation="$(DOCKER_DOCUMENTATION_URL)" \
+		--annotation manifest:org.opencontainers.image.source="$(DOCKER_SOURCE_URL)" \
 		--annotation manifest:org.opencontainers.image.revision="$(R2_SNAP_COMMIT)" \
 		--annotation manifest:org.opencontainers.image.base.name="$(BASE_IMAGE)"
 
-DOCKER_ANNOTATIONS_INDEX:= \
-		--annotation index:org.opencontainers.image.title="radare2" \
+# Docker annotations for the repository index referencing all platforms for a specific tag
+# Note: Do not include ".image.base.*" since it is different for each platform
+DOCKER_ANNOTATIONS_INDEX= \
+		--annotation index:org.opencontainers.image.title="$(DOCKER_TITLE)" \
 		--annotation index:org.opencontainers.image.description="$(DOCKER_DESCRIPTION)" \
 		--annotation index:org.opencontainers.image.ref.name="$(REGISTRY_IMAGE)" \
 		--annotation index:org.opencontainers.image.version="$(R2_VERSION)" \
-		--annotation index:org.opencontainers.image.vendor="radareorg" \
-		--annotation index:org.opencontainers.image.licenses="LGPL-3.0-only" \
-		--annotation index:org.opencontainers.image.url="https://www.radare.org/" \
-		--annotation index:org.opencontainers.image.documentation="https://github.com/radareorg/radare2-snap/blob/main/README-containers.md" \
-		--annotation index:org.opencontainers.image.source="https://github.com/radareorg/radare2-snap" \
+		--annotation index:org.opencontainers.image.vendor="$(DOCKER_VENDOR)" \
+		--annotation index:org.opencontainers.image.licenses="$(DOCKER_LICENSES)" \
+		--annotation index:org.opencontainers.image.url="$(DOCKER_HOMEPAGE_URL)" \
+		--annotation index:org.opencontainers.image.documentation="$(DOCKER_DOCUMENTATION_URL)" \
+		--annotation index:org.opencontainers.image.source="$(DOCKER_SOURCE_URL)" \
 		--annotation index:org.opencontainers.image.revision="$(R2_SNAP_COMMIT)"
