d0a393a29490f7c8a13964b3bd23c6e6e2b85d6e
Pau RE <prodrigestivill@gmail.com>
2024-04-25 10:04:10 +0200

    Update to use Ubuntu 24.04 in supported platforms

--- config/armhf.mk
+++ config/armhf.mk
@@ -1,5 +1,5 @@
-BASE_IMAGE=docker.io/library/ubuntu:22.04
-BASE_SNAP=core22
+BASE_IMAGE=docker.io/library/ubuntu:24.04
+BASE_SNAP=core24
 
 TARGETPLATFORM=linux/arm/v7
 MULTIARCH=arm-linux-gnueabihf
--- config/ppc64el.mk
+++ config/ppc64el.mk
@@ -1,5 +1,5 @@
-BASE_IMAGE=docker.io/library/ubuntu:22.04
-BASE_SNAP=core22
+BASE_IMAGE=docker.io/library/ubuntu:24.04
+BASE_SNAP=core24
 
 TARGETPLATFORM=linux/ppc64le
 MULTIARCH=powerpc64le-linux-gnu
